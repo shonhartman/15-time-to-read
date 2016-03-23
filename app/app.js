2d3ed8f877d16e9e02b7ea1f754f9205b2b1bcd7
Shaun Hartman <shaunhartman@Shauns-MacBook-Pro.local>
2016-03-23 15:51:42 -0500

    rough draft

---
+++ app/app.js
@@ -0,0 +1,7 @@
+let wordCount = document.querySelector("p").innerHTML.length;
+
+function total(wordCount, avg) {
+  return wordCount / 230;
+}
+
+document.createElement("h3").push("#minutes-read");
